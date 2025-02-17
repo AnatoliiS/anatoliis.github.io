3a9de2779802a403712dcb03b7580c02f567fd29
Anatolii S. <Sobolev.Anatoly@gmail.com>
2025-02-17 10:49:59 +0000

    Minify JavaScript output and update script reference in index.pug

--- scripts/render-scripts.js
+++ scripts/render-scripts.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const packageJSON = require('../package.json');
 const upath = require('upath');
 const sh = require('shelljs');
+const uglifyjs = require('uglify-js');
 
 module.exports = function renderScripts() {
 
@@ -12,15 +13,15 @@ module.exports = function renderScripts() {
     sh.cp('-R', sourcePath, destPath)
 
     const sourcePathScriptsJS = upath.resolve(upath.dirname(__filename), '../src/js/scripts.js');
-    const destPathScriptsJS = upath.resolve(upath.dirname(__filename), '../dist/js/scripts.js');
+    const destPathScriptsJS = upath.resolve(upath.dirname(__filename), '../dist/js/scripts.min.js');
     
     const copyright = `/*!
 * ${packageJSON.title} v${packageJSON.version} (${packageJSON.homepage})
-* Copyright 2013-${new Date().getFullYear()} ${packageJSON.author}
+* Copyright 2011-${new Date().getFullYear()} ${packageJSON.author}
 * Licensed under ${packageJSON.license} (${packageJSON.homepage}/blob/master/LICENSE)
 */
 `
-    const scriptsJS = fs.readFileSync(sourcePathScriptsJS);
+    const scriptsJS = uglifyjs.minify(fs.readFileSync(sourcePathScriptsJS, 'utf8')).code;
     
     fs.writeFileSync(destPathScriptsJS, copyright + scriptsJS);
 };
